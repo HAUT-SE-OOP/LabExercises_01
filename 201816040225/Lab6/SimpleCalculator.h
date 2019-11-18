1ed327b4ff36b1dfc32236bd6ca146266bea3e79
201816040225 <56640487+201816040225@users.noreply.github.com>
2019-11-18 21:52:56 +0800

    Create SimpleCalculator.h

---
+++ 201816040225/Lab6/SimpleCalculator.h
@@ -0,0 +1,15 @@
+//Lab 6:SimpleCalculator.h
+class SimpleCalculator
+{
+public:
+
+    double add(double,double)const;//add function
+    double subtract(double,double)const;//subtraction function
+    double multiply(double,double)const;//multiply function
+    double divide(double,double)const;//divide function
+private://data member
+    double number1;
+    double number2;
+
+
+};
