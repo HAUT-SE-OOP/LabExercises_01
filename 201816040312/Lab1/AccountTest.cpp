52fa65422f8adbd6fba00f9686d0c1e34af678bc
201816040312 <56497125+201816040312@users.noreply.github.com>
2019-11-18 19:51:21 +0800

    Create AccountTest.cpp
    This is my LabExercises1
    Student ID:201816040312

---
+++ 201816040312/Lab1/AccountTest.cpp
@@ -0,0 +1,49 @@
+// Lab 1: AccountTest.cpp
+// Create and manipulate Account objects.
+#include <iostream>
+using namespace std;
+
+// include definition of class Account from Account.h
+#include "Account.h"
+
+// function main begins program execution
+int main()
+{
+   Account account1( 50 ); // create Account object
+   Account account2( 0 ); // create Account object
+
+   // display initial balance of each object
+   cout << "account1 balance: $" << account1.getBalance() << endl;
+   cout << "account2 balance: $" << account2.getBalance() << endl;
+
+   int withdrawalAmount; // stores withdrawal amount read from user
+
+   cout << "\nEnter withdrawal amount for account1: "; // prompt
+   cin >> withdrawalAmount; // obtain user input
+   cout << "\nsubtracting " << withdrawalAmount
+      << " from account1 balance\n\n";
+   /* write code to withdraw money from account1 */
+   int flag;
+   flag= account1.debit(withdrawalAmount);
+   if(flag==0)
+   account1.credit(-withdrawalAmount);
+   if(flag==1)
+    cout<<"Debit amount exceeded account balance.\n"<<endl;
+   // display balances
+   cout << "account1 balance: $" << account1.getBalance() << endl;
+   cout << "account2 balance: $" << account2.getBalance() << endl;
+
+   cout << "\nEnter withdrawal amount for account2: "; // prompt
+   cin >> withdrawalAmount; // obtain user input
+   cout << "\nsubtracting " << withdrawalAmount
+      << " from account2 balance\n\n";
+   /* write code to withdraw money from account2 */
+   flag= account2.debit(withdrawalAmount);
+   if(flag==0)
+   account2.credit(-withdrawalAmount);
+   if(flag==1)
+    cout<<"Debit amount exceeded account balance.\n"<<endl;
+   // display balances
+   cout << "account1 balance: $" << account1.getBalance() << endl;
+   cout << "account2 balance: $" << account2.getBalance() << endl;
+} // end main
