3cf26a73bfd98af813589d300709010c93c6c12c
sllthor <56503005+sllthor@users.noreply.github.com>
2019-11-19 18:25:24 +0800

    Create Account.cpp
    This is my Lab1
    Student ID: 201816040110

---
+++ 201816040110/Lab1/Account.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include "Account.h"
+Account::Account( int initialBalance )
+{
+   balance = 0; // assume that the balance begins at 0
+
+   // if initialBalance is greater than 0, set this value as the
+   // balance of the Account; otherwise, balance remains 0
+   if ( initialBalance > 0 )
+      balance = initialBalance;
+
+   // if initialBalance is negative, print error message
+   if ( initialBalance < 0 )
+      cout << "Error: Initial balance cannot be negative.\n" << endl;
+} // end Account constructor
+
+// credit (add) an amount to the account balance
+void Account::credit( int amount )
+{
+   if((balance-amount)<0)
+    {
+        cout << "Error : Initial balance is not that much.\n"<< endl;
+    }
+    else
+    {
+        balance =balance -amount;
+    }
+    // add amount to balance
+} // end function credit
+
+/* write code to define member function debit. */
+
+// return the account balance
+int Account::getBalance()
+{
+   return balance; // gives the value of balance to the calling function
+} // end function getBalance
