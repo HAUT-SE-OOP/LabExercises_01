0221e4cb75952ee4cec39afcc627627f35a67954
Lee Hao <56342291+L1hao@users.noreply.github.com>
2019-11-19 13:24:13 +0800

    Create EmployeeTest.cpp

---
+++ 201816040207/Lab3/EmployeeTest.cpp
@@ -0,0 +1,24 @@
+// Lab 3: EmployeeTest.cpp
+// Create and manipulate two Employee objects.
+#include <iostream>
+using namespace std;
+
+#include "Employee.h" // include definition of class Employee
+
+// function main begins program execution
+int main()
+{
+   /* Create two Employee objects and assign them to Employee variables. */
+    Employee employee1("Bob Jones","Bob",34500);
+    Employee employee2("Susan Baker","Susan",37800);
+   /* Output the first name, last name and salary for each Employee. */
+    cout<<"Employee1:"<<employee1.getname()<<";"<<"Yearly Salary:"<<employee1.getsalary()<<endl;
+    cout<<"Employee2:"<<employee2.getname()<<";"<<"Yearly Salary:"<<employee2.getsalary()<<endl;
+   /* Give each Employee a 10% raise. */
+    employee1.salarychage(0.1);
+    employee2.salarychage(0.1);
+   /* Output the first name, last name and salary of each Employee again. */
+    cout<<"Employee1:"<<employee1.getname()<<";"<<"Yearly Salary:"<<employee1.getsalary()<<endl;
+    cout<<"Employee2:"<<employee2.getname()<<";"<<"Yearly Salary:"<<employee2.getsalary()<<endl;
+} // end main
+
