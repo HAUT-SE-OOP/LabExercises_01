4737a9e823cc12381062fda1cdbc4d3c6f042125
David-0424 <56601563+David-0424@users.noreply.github.com>
2019-11-20 22:51:59 +0800

    Create SimpleCalculator.h

---
+++ 201816040105/Lab6/SimpleCalculator.h
@@ -0,0 +1,11 @@
+// Lab Exercise 6: SimpleCalculator.h
+
+// class SimpleCalculator definition
+class SimpleCalculator
+{
+public: 
+   double add( double, double) const;
+   double subtract( double, double ) const; 
+   double multiply( double, double ) const;
+   double divide( double, double) const;
+}; // end class SimpleCalculator
