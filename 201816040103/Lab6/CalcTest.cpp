eb86fac4585633da8ad1b15d975352edfc8ed1cc
201816040103 <56601097+201816040103@users.noreply.github.com>
2019-11-20 22:10:34 +0800

    Create CalcTest.cpp

---
+++ 201816040103/Lab6/CalcTest.cpp
@@ -0,0 +1,13 @@
+// Lab Exercise 6: CalcTest.cpp
+
+#include <iostream>
+using namespace std; 
+
+#include "SimpleCalculator.h"
+
+int main()
+{
+	SimpleCalculator sc;
+	sc.input();
+	sc.printValues();
+}
