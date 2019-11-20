b6b33e87560729dc75d3b5431cabb2d2ff9a6a23
201816040103 <56601097+201816040103@users.noreply.github.com>
2019-11-20 22:06:33 +0800

    Create ComplexTest.cpp

---
+++ 201816040103/Lab4/ComplexTest.cpp
@@ -0,0 +1,13 @@
+// Lab 4: ComplexTest.cpp
+#include <iostream> 
+using namespace std; 
+
+#include "Complex.h"
+using namespace std;
+
+int main()
+{
+   Complex a; // create three Complex objects 
+   cout<<a.setComplexNumber();
+} // end main
+
