a5d6da08f0bd316d238b88639e96d381f57fd50f
gugugugugugu-guer <56782061+gugugugugugu-guer@users.noreply.github.com>
2019-11-20 19:16:48 +0800

    Create SimpleCalculator.h

---
+++ 201816040323/Lab6/SimpleCalculator.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// class SimpleCalculator definition
+class SimpleCalculator
+{
+public:
+   double add( double, double ) const;
+   double divide( double, double ) const;
+   double subtract( double, double ) const;
+   double multiply( double, double ) const;
+}; // end class SimpleCalculator
