ff4c4eff342c306ffbb0d401d45a1124699402cf
gugugugugugu-guer <56782061+gugugugugugu-guer@users.noreply.github.com>
2019-11-20 19:16:02 +0800

    Update SimpleCalculator.cpp

--- 201816040323/Lab6/SimpleCalculator.cpp
+++ 201816040323/Lab6/SimpleCalculator.cpp
@@ -1,11 +1,28 @@
-#include<iostream>
-using namespace std;
-// class SimpleCalculator definition
-class SimpleCalculator
+#include "SimpleCalculator.h"
+
+
+// function subtract definition
+double SimpleCalculator::subtract( double a, double b ) const
 {
-public:
-   double add( double, double ) const;
-   double divide( double, double ) const;
-   double subtract( double, double ) const;
-   double multiply( double, double ) const;
-}; // end class SimpleCalculator
+   return a - b;
+} // end function subtract
+double SimpleCalculator::add( double a, double b ) const
+{
+   return a + b;
+}
+double SimpleCalculator::divide( double a, double b ) const
+{
+    if(b==0)
+    {
+        cout << "The denominator can't be 0 "<<endl;//If b=0 then print The denominator can't be 0
+    }
+    else
+    {
+        return a / b;
+    }
+}
+// function multiply definition
+double SimpleCalculator::multiply( double a, double b ) const
+{
+   return a * b;
+} // end function multiply
