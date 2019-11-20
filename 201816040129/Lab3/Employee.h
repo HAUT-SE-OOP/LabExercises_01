4989b8cb1a454e7a62810278fdc7b8f3ced0473e
201816040129 <56758137+201816040129@users.noreply.github.com>
2019-11-20 23:39:10 +0800

    Create Employee.h
    This is my LabExercises_01
    Student ID:201816040129

---
+++ 201816040129/Lab3/Employee.h
@@ -0,0 +1,29 @@
+// Lab 3: Employee.h
+// Employee class definition.
+
+#include <string> // program uses C++ standard string class
+using namespace std;
+
+// Employee class definition
+class Employee
+{
+public:
+   Employee( string, string,int );
+   /* Declare a constructor that has one parameter for each data member */
+   void setFirstName( string );
+   /* Declare a set method for the employee's first name */
+   string getFirstName();
+   /* Declare a get method for the employee's first name */
+   void setLastName(string);
+   /* Declare a set method for the employee's last name */
+   string getLastName();
+   /* Declare a get method for the employee's last name */
+   void setSalary(int);
+   /* Declare a set method for the employee's monthly salary */
+   int getSalary();
+   /* Declare a get method for the employee's monthly salary */
+private:
+   string FirstName;/* Declare a string data member for the employee's first name */
+   string LastName;/* Declare a string data member for the employee's last name */
+   int Salary;/* Declare an int data member for the employee's monthly salary */
+}; // end class Employee
