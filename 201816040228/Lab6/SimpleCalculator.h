7c811057c27969e2ecbe63d018c77cdd6bdcc984
yangcuicui12 <56767779+yangcuicui12@users.noreply.github.com>
2019-11-20 22:36:39 +0800

    Create SimpleCalculator.h

---
+++ 201816040228/Lab6/SimpleCalculator.h
@@ -0,0 +1,14 @@
+// Lab Exercise 6: SimpleCalculator.h
+
+// class SimpleCalculator definition
+class SimpleCalculator
+{
+public:
+   //add member function
+   double add( double, double ) const;
+   double subtract( double, double ) const;
+   double multiply( double, double ) const;
+   double divide( double, double ) const;
+   //divide member function
+}; // end class SimpleCalculator
+
