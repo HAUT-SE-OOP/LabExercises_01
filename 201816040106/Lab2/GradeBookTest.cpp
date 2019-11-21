957768326ad156873128dd095e7b3e6d5ca4a738
numei <53113809+numei@users.noreply.github.com>
2019-11-21 18:41:01 +0800

    Create GradeBookTest.cpp
    This is my LabExercise_01
    My Student ID:201816040106

---
+++ 201816040106/Lab2/GradeBookTest.cpp
@@ -0,0 +1,24 @@
+// Lab 2: GradeBookTest.cpp
+// Test program for modified GradeBook class.
+#include <iostream>
+using namespace std; 
+
+// include definition of class GradeBook from GradeBook.h
+#include "GradeBook.h"
+
+// function main begins program execution
+int main()
+{
+   // create a GradeBook object; pass a course name and instructor name
+   GradeBook gradeBook("CS101 Introduction to C++ Programming","Sam Smith");
+   // display welcome message and instructor's name
+   gradeBook.displayMessage(); 
+   cout<<endl;
+   /*change instructor's name to Judy Jones*/
+   cout<<"Changing instructor name to Judy Jones"<<endl;
+   cout<<endl;
+   gradeBook.setInstructorName("Judy Jones");
+   /*output changes */
+   gradeBook.displayMessage(); 
+   return 0;
+} // end main
