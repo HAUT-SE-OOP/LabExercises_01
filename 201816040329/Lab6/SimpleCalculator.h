9d25f4ad842ad0780cc304ee322584fc5c27fef2
kennnna <56779636+201816040329@users.noreply.github.com>
2019-11-21 17:10:35 +0800

    Create SimpleCalculator.h

---
+++ 201816040329/Lab6/SimpleCalculator.h
@@ -0,0 +1,9 @@
+// class SimpleCalculator definition
+class SimpleCalculator
+{
+public:
+   double add(double,double)const;
+   double subtract( double, double ) const;
+   double multiply( double, double ) const;
+   double divide(double,double)const;
+}; // end class SimpleCalculator
