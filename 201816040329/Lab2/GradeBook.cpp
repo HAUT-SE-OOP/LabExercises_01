66e8023b1445c3a94c39b65156ec51b2a91f3ad2
kennnna <56779636+201816040329@users.noreply.github.com>
2019-11-21 16:57:46 +0800

    Create GradeBook.cpp

---
+++ 201816040329/Lab2/GradeBook.cpp
@@ -0,0 +1,40 @@
+// Member-function definitions for class GradeBook.
+#include <iostream>
+using namespace std;
+
+// include definition of class GradeBook from GradeBook.h
+#include "GradeBook.h"
+
+// constructor initializes courseName and instructorName
+// with strings supplied as arguments
+GradeBook::GradeBook( string course, string instructor )
+{
+   setCourseName( course ); // initializes courseName
+   setInstructorName( instructor ); // initialiZes instructorName
+} // end GradeBook constructor
+
+// function to set the course name
+void GradeBook::setCourseName( string name )
+{
+   courseName = name; // store the course name
+} // end function setCourseName
+
+// function to retrieve the course name
+string GradeBook::getCourseName()
+{
+   return courseName;
+} // end function getCourseName
+
+string GradeBook::getInstructorName()
+{
+   return instructorName;
+}
+
+// display a welcome message and the instructor's name
+void GradeBook::displayMessage()
+{
+   // display a welcome message containing the course name
+   cout << "Welcome to the grade book for\n" << getCourseName() << "!"
+      << endl;
+   cout << "this course is presented by;" <<getInstructorName() << endl;
+} // end function displayMessage
