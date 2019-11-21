09087c2b8acdb92585d5f8de0884b5ec03019289
XueLing-c <56784633+XueLing-c@users.noreply.github.com>
2019-11-21 09:54:32 +0800

    Create Complex.h
    This is my lab 01
    Student ID:201816040114

---
+++ 201816040114/lab4/Complex.h
@@ -0,0 +1,20 @@
+// Lab 4: Complex.h
+#ifndef COMPLEX_H
+#define COMPLEX_H
+class Complex
+{
+public:
+    explicit Complex(double=0.0,double=0.0);
+    void printComplex();
+    void setComplexNumber(double,double);
+    Complex add(const Complex &right);
+    Complex subtract(const Complex &right);
+private:
+    double realPart;
+    double imaginaryPart;
+};
+
+/* Write class definition for Complex */
+
+#endif
+
