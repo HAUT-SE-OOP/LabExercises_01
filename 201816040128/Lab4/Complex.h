488c168fdb8206fe7fceca46fbbc36b0ac9e733e
201816040128尚莹莹 <56549207+201816040128@users.noreply.github.com>
2019-11-21 21:53:08 +0800

    Create Complex.h

---
+++ 201816040128/Lab4/Complex.h
@@ -0,0 +1,18 @@
+// Lab 4: Complex.h
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+/* Write class definition for Complex */
+class Complex{
+public:
+	Complex(double, double);
+	Complex add(const Complex& );
+	Complex subtract(const Complex& );
+	void printComplex();
+	void setComplexNumber(double , double );
+private:
+	double real ;
+	double imaginary;
+};
+#endif
+
