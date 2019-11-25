1ffffc53638e2c5d1688fba5b2da159494d62da7
201816040127 <56643454+201816040127@users.noreply.github.com>
2019-11-25 19:10:45 +0800

    Create Lab1
    This is my assignment_01
    Student ID:201816040127

---
+++ Account.h
@@ -0,0 +1,14 @@
+// Lab 1: Account.h
+// Definition of Account class.
+
+class Account
+{
+public:
+   Account( int ); // constructor initializes balance
+   void credit( int ); // add an amount to the account balance
+   void debit( int );//withdraw the money from account balance
+   int getBalance(); // return the account balance
+private:
+   int balance; // data member that stores the balance
+}; // end class Account
+
