4c8eef8b0ee0e836bf9c7c8875ff940d958c2141
201826030127杨慧姗 <56498911+201826030127@users.noreply.github.com>
2019-11-27 19:02:29 +0800

    Create SimpleCalculatorTest.cpp
    This is my Lab6.
    Student ID:201826030127

---
+++ 201826030127/Lab6/SimpleCalculatorTest.cpp
@@ -0,0 +1,33 @@
+// Lab Exercise 6: CalcTest.cpp
+
+#include <iostream>
+using namespace std;
+
+#include "SimpleCalculator.h"
+
+int main()
+{
+
+   double a = 10.0;
+   double b = 20.0;
+   SimpleCalculator sc;
+   /* Instantiate an object of type Simplecalculator */
+   cout << "The value of a is: " << a << "\n";
+   cout << "The value of b is: " << b << "\n\n";
+
+   double addition=sc.add(a,b);/* Write a line that adds a and b through your SimpleCalculator
+      object; assign the result to a variable named addition */
+   cout << "Adding a and b yields " << addition << "\n";
+
+   double subtraction = sc.subtract( a, b );
+   cout << "Subtracting b from a yields " << subtraction << "\n";
+
+   double multiplication = sc.multiply( a, b );
+   cout << "Multiplying a by b yields " << multiplication << "\n";
+
+   double division=sc.divide(a,b);
+   if(b==0)
+    cout<<"ERROR!";
+    else
+   cout << "Dividing a by b yields " << division << endl;
+}
